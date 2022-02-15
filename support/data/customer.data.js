4b638cb2a6527fe493850a66d812df8bd13ff206
Godswill Ayewah <c-godswill.ayewah@cart.com>
2022-02-14 22:21:42 -0600

    data files for the specs

---
+++ support/data/customer.data.js
@@ -0,0 +1,7 @@
+
+export let customerData = {
+    "title": "Edit Customer",
+    "sucMsg": "Saved successfully",
+    "phoneFormat": "9#########",
+    "alertMsg": "Are you sure you want to delete this customer and all of their orders, messages and history?"
+}
---
+++ support/data/order.data.js
@@ -0,0 +1,13 @@
+
+export let orderData = {
+    "orderTitle": "Orders",
+    'editOrderTitle': "Order Edit"
+}
+
+export let orderStatus = {
+    "cancelOrder": "Cancel Order"
+}
+
+export let orderSaveMsg = {
+    "sucMsg": "Saved Successfully"
+}
