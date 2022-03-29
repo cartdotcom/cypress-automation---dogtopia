8b645c36de9e30fe781df0b3eb5d6736fb72edf5
Godswill Ayewah <c-godswill.ayewah@cart.com>
2022-03-28 22:19:05 -0500

    data for cc info and order status

--- support/data/order.data.js
+++ support/data/order.data.js
@@ -5,9 +5,18 @@ export let orderData = {
 }
 
 export let orderStatus = {
-    "cancelOrder": "Cancel Order"
+    "cancelOrder": "Cancel Order",
+    "creditDeclined": "Credit Declined",
+    "awaitingPayment": "Awaiting Payment"
 }
 
 export let orderSaveMsg = {
     "sucMsg": "Saved Successfully"
+}
+
+export let invalidCC = {
+    "ccNum": "5448280034000007",
+    "ccName": "Test User",
+    "ccExp": "01/29",
+    "ccCvv": "123"
 }
